5e74f50fb8a6bb803112750c5a42265df502d3d1
FabrizioCmp <fabriziocampari01@gmail.com>
2023-02-01 17:23:02 +0100

    adds edit links in project.index page

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -11,6 +11,7 @@
                     <th>Nome Progetto</th>
                     <th>Cover</th>
                     <th>Link github</th>
+                    <th>Manage</th>
                 </tr>
             </thead>
             <tbody>
@@ -20,9 +21,16 @@
                         <td>{{ $project->name }}</td>
                         <td>{{ $project->cover_img }}</td>
                         <td>{{ $project->github_link }}</td>
+                        <td class="">
+                            <a class="me-2" href="#">edit</a>
+                            <a class="text-danger" href="#">delete</a>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+        <div class="w-100 text-center">
+            <a class="btn btn-success " href="#">new Project</a>
+        </div>
     </div>
 @endsection
